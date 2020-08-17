3269c3cdfab8b0df246079a050d38ff7fd90440e
coding-with-AHRK <69730619+coding-with-AHRK@users.noreply.github.com>
2020-08-17 13:54:12 -0700

    Add files via upload

---
+++ simple-calculator.py
@@ -0,0 +1,11 @@
+ #we are going to take 2 input from user and due to
+#user input is by defualt comes in string datatypes so first we convert it into int and thn store
+#it into variable named val1 and thn do it further +,-,*,/,% calculation
+
+val1 = int(input("enter first number: "))
+val2 = int(input("enter second number: "))
+print("val1 + val2 = ", val1+val2)
+print("val1 - val2 = ", val1- val2)
+print("val1 * val2 = ", val1 * val2)
+print("val1 / val2 = ", val1 / val2)
+print("val1 % val2 = ", val1 % val2)
